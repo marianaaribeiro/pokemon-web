8f5923903c2823c2e4ef97fd8cc7a5356565b5b2
Mariana Ali <mariana.ribeiro-ext@viavarejo.com.br>
2023-02-22 05:25:34 -0300

    update api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosError, AxiosRequestConfig } from 'axios'
 
-const API_URL = '${VITE_API_URL}'
+const API_URL = import.meta.env.VITE_API_URL ?? '${VITE_API_URL}'
 
 export const api = axios.create({
   baseURL: API_URL,
